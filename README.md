fe343c5478ddfd86c942c588f3c3c2df5264ac98
catpea <corporate.miou@gmail.com>
2021-05-22 12:53:43 -0400

    New Release

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # wibble
 Convert a repository to a useful graph.
+
+Used in creating cytoscape.js graphs.
---
+++ index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+//
+
+  -- scan all files,
+  -- extract tags
+  -- create graph
+
+//

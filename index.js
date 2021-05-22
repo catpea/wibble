7ba7535766ce15934314462e2ef29cf57dc333eb
catpea <corporate.miou@gmail.com>
2021-05-22 13:04:12 -0400

    New Release

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
+import util from 'util';
+const glob = util.promisify((await import("glob")).default);
 
-//
+// scan all files,
+// extract tags
+// create graph
 
-  -- scan all files,
-  -- extract tags
-  -- create graph
-
-//
+const files = await glob("**/*.js", {});
+console.log(files);
